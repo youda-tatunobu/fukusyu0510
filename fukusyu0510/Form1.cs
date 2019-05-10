1431793af970fd9f77bf121e3adc830f11d8caa9
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-05-10 13:57:46 +0900

    Initial commit

---
+++ fukusyu0510/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukusyu0510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.ForeColor = Color.Red;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"tod.bmp";
+        }
+    }
+}
